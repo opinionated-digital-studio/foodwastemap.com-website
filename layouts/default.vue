fa775831baffb653a8953025d289022468bf42aa
Pandu Supriyono <mail@supriyono.nl>
2020-11-25 02:33:48 +0100

    fix(layout): pushed footer to bottom

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
+  <div class="layout-wrapper">
     <FWSMHeader />
     <nuxt />
     <FWSMFooter />
   </div>
 </template>
 
+<style lang="scss" scoped>
+.layout-wrapper {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+.footer {
+  flex-grow: 1;
+}
+</style>
+
 <script lang="ts">
 import FWSMHeader from "~/components/FWSMHeader.vue";
 import FWSMFooter from "~/components/FWSMFooter.vue";
 import Vue from "vue";
 export default Vue.extend({
-  components: { FWSMHeader, FWSMFooter }
+  components: { FWSMHeader, FWSMFooter },
 });
 </script>
