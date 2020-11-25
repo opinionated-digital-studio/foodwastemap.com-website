d408f75edb9a6726d3c3670384d35a3299f48255
Pandu Supriyono <mail@supriyono.nl>
2020-11-25 10:48:38 +0100

    improvement: changed font stack as per client request

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Varela&display=swap' },
       { rel: 'stylesheet', href: 'https://use.typekit.net/otf2qav.css' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
