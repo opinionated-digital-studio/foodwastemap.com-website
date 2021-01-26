da9eaa3ed5a552f8e5342e4639e347aed5b3343f
Pandu Supriyono <mail@supriyono.nl>
2021-01-26 20:10:17 +0100

    fix: links in CTA

--- components/FWSMCallToAction.vue
+++ components/FWSMCallToAction.vue
@@ -5,8 +5,8 @@
         <h3 class="title is-2">
           Put your organization on the map
         </h3>
-        <a href="" class="button is-white">Sign up</a>
-        <a href="" class="button is-white is-outlined">Contact us</a>
+        <a href="/sign-up" class="button is-white">Maak een account aan</a>
+        <a href="mailto:mail@foodwastemap.com" class="button is-white is-outlined">Contact us</a>
       </div>
     </div>
   </section>
